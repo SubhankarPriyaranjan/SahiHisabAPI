e65c7777142979960c6c004f9598751068a9d52c
Subhankar Priyaranjan <subhankar.rout@smaketsolutions.com>
2024-09-12 17:38:36 +0530

    Add project files.

---
+++ Data/DataAccess.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SahiHisabAPI.Data
+{
+    public class DataAccess : DbContext
+        //DbContext is a collection of DB set its helps to communicate with Database 
+    {
+        public DataAccess(DbContextOptions<DataAccess> options):base(options) 
+        {
+        
+        }
+
+        // Here are the Model Class 
+        //like this public Dbset<Model Name ...> Model NAme .. {get; set;}
+
+    }
+}
